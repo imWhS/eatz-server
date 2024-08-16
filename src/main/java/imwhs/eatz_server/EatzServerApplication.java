bb46272712f329f0896f4a8609d1a5ede3179432
imWhS <heextory@icloud.com>
2024-08-16 20:52:11 +0900

    Initial commit

---
+++ src/main/java/imwhs/eatz_server/EatzServerApplication.java
@@ -0,0 +1,13 @@
+package imwhs.eatz_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatzServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatzServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/imwhs/eatz_server/EatzServerApplicationTests.java
@@ -0,0 +1,13 @@
+package imwhs.eatz_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatzServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
