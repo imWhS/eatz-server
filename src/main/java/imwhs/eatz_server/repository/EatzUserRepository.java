e7272b166d6264d74c79c47f9bdd228d90564155
imWhS <heextory@icloud.com>
2024-08-20 22:26:04 +0900

    feat: EatzUserRepository에 Spring Data JPA 적용

--- src/main/java/imwhs/eatz_server/repository/EatzUserRepository.java
+++ src/main/java/imwhs/eatz_server/repository/EatzUserRepository.java
@@ -1,13 +1,15 @@
 package imwhs.eatz_server.repository;
 
+import imwhs.eatz_server.domain.EatzUser;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  * EatzUserRepository
  */
 @Repository
-public class EatzUserRepository {
+public interface EatzUserRepository extends JpaRepository<EatzUser, Long> {
 
-    
+
 
 }
