f8b1aa9ac0c76a3004ff79fb36de34c12a0ad061
imWhS <heextory@icloud.com>
2024-08-20 22:42:41 +0900

    docs: EatzUser, Recipe 엔티티 및 EatzUserRepository 주석 보강

--- src/main/java/imwhs/eatz_server/domain/Role.java
+++ src/main/java/imwhs/eatz_server/domain/Role.java
@@ -1,11 +1,21 @@
 package imwhs.eatz_server.domain;
 
 /**
- * Role은 사용자 역할을 구분하기 위해 사용합니다.
+ * Role 열거형.<br/>
+ * <p>
+ * 사용자 역할을 정의합니다.<br/>
+ * 사용자는 MEMBER(회원), ADMIN(관리자) 중 하나의 역할을 가질 수 있씁니다.
  */
 public enum Role {
 
+    /**
+     * 회원 역할
+     */
     MEMBER,
+
+    /**
+     * 관리자 역할
+     */
     ADMIN
 
 }
