46794f81840812fedb47e5abe0de1fc57b16db28
imWhS <heextory@icloud.com>
2024-08-21 22:20:35 +0900

    feat: 기본적인 사용자 등록, 조회 기능 및 예외 처리 구현

--- src/main/java/imwhs/eatz_server/repository/EatzUserRepository.java
+++ src/main/java/imwhs/eatz_server/repository/EatzUserRepository.java
@@ -4,6 +4,8 @@ import imwhs.eatz_server.domain.EatzUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * EatzUserRepository 리포지토리.<br/>
  * <p>
@@ -11,4 +13,16 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface EatzUserRepository extends JpaRepository<EatzUser, Long> {
+
+    /**
+     * 사용자 이름으로 엔티티 조회.
+     * <p>
+     * 사용자 이름으로 단일 EatzUser 엔티티를 조회합니다.
+     * 조회할 엔티티가 없으면 Optional을 반환합니다.
+     *
+     * @param username 조회할 사용자 이름.
+     * @return 사용자 이름에 해당하는 EatzUser 엔티티(Optional).
+     */
+    Optional<EatzUser> findByUsername(String username);
+
 }
