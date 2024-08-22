6b24a171192fb6b8859dc6da42dbdfc169093772
imWhS <heextory@icloud.com>
2024-08-22 12:35:40 +0900

    feat: 사용자 정보 수정 기능 추가

--- src/main/java/imwhs/eatz_server/service/EatzUserService.java
+++ src/main/java/imwhs/eatz_server/service/EatzUserService.java
@@ -1,6 +1,7 @@
 package imwhs.eatz_server.service;
 
 import imwhs.eatz_server.domain.EatzUser;
+import imwhs.eatz_server.dto.UpdateEatzUserDTO;
 import imwhs.eatz_server.exception.EatzUserNotFoundException;
 import imwhs.eatz_server.repository.EatzUserRepository;
 import lombok.RequiredArgsConstructor;
@@ -36,13 +37,13 @@ public class EatzUserService {
     }
 
     /**
-     * 사용자 이름로 사용자 조회.
+     * 사용자 이름으로 사용자 조회.
      * <p>
      * username으로 특정 EatzUser를 조회합니다.
      */
     public EatzUser findUserByUsername(String username) {
         return userRepository.findByUsername(username)
-                .orElseThrow(() -> new EatzUserNotFoundException("사용자 이름이 " + username + "인 사용자를 찾을 수 없습니다."))
+                .orElseThrow(() -> new EatzUserNotFoundException("사용자 이름이 " + username + "인 사용자를 찾을 수 없습니다."));
     }
 
     /**
@@ -50,6 +51,15 @@ public class EatzUserService {
      */
     public List<EatzUser> findAllUsers() {
         return userRepository.findAll();
+    }
+
+    /**
+     * 사용자 정보 수정.
+     */
+    @Transactional(readOnly = false)
+    public void updateUser(Long id, UpdateEatzUserDTO dto) {
+        EatzUser user = userRepository.findById(id).orElseThrow(() -> new EatzUserNotFoundException("id가 " + id + "인 사용자를 찾을 수 없습니다."));
+        user.update(dto.getUsername(), dto.getEmail(), dto.getPassword());
     }
 
 }
