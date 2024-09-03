d625a531ef5e983b7b4d54ec14fca974f244b9cd
imWhS <heextory@icloud.com>
2024-09-03 13:30:07 +0900

    feat: 레시피 목록 응답 시 페이징 처리 기능 추가

--- src/main/java/imwhs/eatz_server/repository/RecipeRepository.java
+++ src/main/java/imwhs/eatz_server/repository/RecipeRepository.java
@@ -1,6 +1,9 @@
 package imwhs.eatz_server.repository;
 
+import imwhs.eatz_server.domain.EatzUser;
 import imwhs.eatz_server.domain.Recipe;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +15,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
-
+    Page<Recipe> findByUser(EatzUser user, PageRequest pageRequest);
 
 }
