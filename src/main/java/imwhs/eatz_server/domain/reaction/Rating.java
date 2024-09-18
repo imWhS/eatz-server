db3961fdda590773c50eb25905a69819e311de69
imWhS <heextory@icloud.com>
2024-09-18 20:10:22 +0900

    feat: ReactionService를 통한 Reaction Create, Update 구현

--- src/main/java/imwhs/eatz_server/domain/reaction/Rating.java
+++ src/main/java/imwhs/eatz_server/domain/reaction/Rating.java
@@ -1,12 +1,28 @@
 package imwhs.eatz_server.domain.reaction;
 
+import imwhs.eatz_server.domain.EatzUser;
+import imwhs.eatz_server.domain.Recipe;
+import imwhs.eatz_server.dto.UpdateRatingDto;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.Getter;
 
+@Getter
 @Entity
-@DiscriminatorValue("COMMENT")
+@DiscriminatorValue("RATING")
 public class Rating extends Reaction {
 
     private Integer score;
+
+    public Rating() {}
+
+    public Rating(Recipe recipe, EatzUser user, Integer score) {
+        super(recipe, user);
+        this.score = score;
+    }
+
+    public void update(UpdateRatingDto dto) {
+        score = dto.getScore();
+    }
 
 }
---
+++ src/main/java/imwhs/eatz_server/dto/UpdateReactionDto.java
@@ -0,0 +1,9 @@
+package imwhs.eatz_server.dto;
+
+import imwhs.eatz_server.domain.reaction.Reaction;
+
+public abstract class UpdateReactionDto {
+
+    protected UpdateReactionDto() {}
+
+}
