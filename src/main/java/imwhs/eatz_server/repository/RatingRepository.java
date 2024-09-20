47d53d293f1a8b875d28ab95239991420c03ee4b
imWhS <heextory@icloud.com>
2024-09-20 23:08:47 +0900

    feat: Comment, Rating 독립 엔티티 구조로 변경

---
+++ src/main/java/imwhs/eatz_server/repository/RatingRepository.java
@@ -0,0 +1,12 @@
+package imwhs.eatz_server.repository;
+
+import imwhs.eatz_server.domain.Rating;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RatingRepository extends JpaRepository<Rating, Long> {
+
+    boolean existsByRecipeIdAndUserId(Long recipeId, Long userId);
+
+}
