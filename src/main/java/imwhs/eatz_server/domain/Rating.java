9894505fc5bdb68eafac37c7706c415d9687b15f
imWhS <heextory@icloud.com>
2024-09-22 17:05:22 +0900

    feat: 댓글, 평가 수정 및 삭제 처리 추가 구현

--- src/main/java/imwhs/eatz_server/domain/Rating.java
+++ src/main/java/imwhs/eatz_server/domain/Rating.java
@@ -1,11 +1,12 @@
 package imwhs.eatz_server.domain;
 
+import imwhs.eatz_server.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 
 @Getter
 @Entity
-public class Rating {
+public class Rating extends BaseEntity {
 
     @Id @GeneratedValue
     private Long id;
@@ -22,11 +23,35 @@ public class Rating {
      * 평가 점수.
      * 평가 점수는 1부터 5 사이의 자연수로만 설정할 수 있습니다.
      */
-    private int score;
+    private Integer score = null;
 
+    /**
+     * 평가 내용.
+     */
+    private String content = null;
+
+    /**
+     * 숨김 여부.
+     */
     private boolean isHidden;
 
     protected Rating() {}
+
+    /**
+     * Rating의 필드 초기화 생성자.
+     * @param user 평가하는 사용자
+     * @param recipe 평가할 레시피
+     * @param score 평가 점수
+     * @param content 평가 내용
+     */
+    public Rating(EatzUser user, Recipe recipe, int score, String content) {
+        validateScore(score);
+
+        this.user = user;
+        this.recipe = recipe;
+        this.score = score;
+        this.content = content;
+    }
 
     /**
      * Rating의 필수 필드 초기화 생성자.
@@ -35,17 +60,57 @@ public class Rating {
      * @param score 평가 점수
      */
     public Rating(EatzUser user, Recipe recipe, int score) {
-        if (score < 1 || score > 5) {
-            throw new IllegalArgumentException("평가 점수는 1부터 5 사이의 자연수여야 합니다.");
-        }
+        this(user, recipe, score, null);
+    }
 
-        this.user = user;
-        this.recipe = recipe;
+    /**
+     * 평가 점수를 수정합니다.
+     * @param score 수정할 평가 점수
+     */
+    public void updateScore(int score) {
+        validateRating();
+        validateScore(score);
+
         this.score = score;
     }
 
-    public void updateScore(int score) {
-        this.score = score;
+    /**
+     * 평가 내용을 수정합니다.
+     * @param content 수정할 평가 내용
+     */
+    public void updateContent(String content) {
+        validateRating();
+        this.content = content;
+    }
+
+    /**
+     * 평가를 삭제 처리합니다.
+     * score, content의 값을 지웁니다.
+     */
+    @Override
+    public void markAsDeleted() {
+        super.markAsDeleted();
+        this.score = null;
+        this.content = null;
+    }
+
+    /**
+     * 평가 점수의 유효성을 검증합니다.
+     * @param score 검증할 평가 점수
+     */
+    private static void validateScore(int score) {
+        if (score < 1 || score > 5) {
+            throw new IllegalArgumentException("평가 점수는 1부터 5 사이의 자연수여야 합니다.");
+        }
+    }
+
+    /**
+     * 평가의 유효성을 검증합니다.
+     */
+    private void validateRating() {
+        if (this.isMarkedAsDeleted()) {
+            throw new IllegalStateException("삭제 처리된 평가입니다.");
+        }
     }
 
 }
--- src/main/java/imwhs/eatz_server/service/CommentService.java
+++ src/main/java/imwhs/eatz_server/service/CommentService.java
@@ -24,6 +24,13 @@ public class CommentService {
 
     private final EatzUserRepository userRepository;
 
+    /**
+     * 새 댓글을 등록합니다.
+     * @param recipeId 댓글을 달 레시피의 ID
+     * @param userId 댓글 등록을 요청한 사용자의 ID
+     * @param content 등록할 댓글의 내용
+     * @return 등록 완료된 댓글의 ID
+     */
     @Transactional
     public Long registerComment(Long recipeId, Long userId, String content) {
         Recipe recipe = recipeRepository.findById(recipeId)
@@ -37,33 +44,50 @@ public class CommentService {
         return comment.getId();
     }
 
+    /**
+     * 댓글을 수정합니다.
+     * @param commentId 수정할 댓글의 ID
+     * @param userId 댓글 수정을 요청한 사용자의 ID
+     * @param content 수정할 댓글의 내용
+     */
     @Transactional
     public void updateComment(Long commentId, Long userId, String content) {
-        Comment comment = findComment(commentId, userId);
+        Comment comment = getComment(commentId, userId);
         comment.updateContent(content);
     }
 
+    /**
+     * 댓글을 삭제 처리합니다.
+     * @param commentId 삭제 처리할 댓글의 ID
+     * @param userId 댓글 삭제 처리를 요청한 사용자의 ID
+     */
     @Transactional
     public void deleteComment(Long commentId, Long userId) {
-        Comment comment = findComment(commentId, userId);
-        commentRepository.delete(comment);
+        Comment comment = getComment(commentId, userId);
+        comment.markAsDeleted();
     }
 
     /**
      * Comment 엔티티를 가져옵니다.
      * @param commentId 댓글 ID
-     * @param userId 서비스를 요청한 사용자 ID
+     * @param userId 엔티티를 요청한 사용자 ID
      * @throws CommentNotFoundException commentId에 해당하는 Comment 엔티티가 존재하지 않을 경우
      * @throws UnauthorizedEatzUserException 댓글을 등록한 사용자의 id가 userId와 일치하지 않을 경우(접근 권한이 없는 사용자의 요청인 경우)
+     * @throws IllegalArgumentException 가져오려는 댓글이 삭제 처리된 경우
      * @return Comment 엔티티
      */
-    private Comment findComment(Long commentId, Long userId) {
+    private Comment getComment(Long commentId, Long userId) {
         Comment comment = commentRepository.findById(commentId)
                 .orElseThrow(() -> new CommentNotFoundException("id가 " + commentId + "인 댓글이 존재하지 않습니다."));
 
         if (!Objects.equals(comment.getUser().getId(), userId)) {
-            throw new UnauthorizedEatzUserException("댓글을 등록한 사용자가 아니어서, 수정을 진행할 수 없습니다.");
+            throw new UnauthorizedEatzUserException("댓글을 등록한 사용자가 아니어서, 권한이 없습니다.");
         }
+
+        if (comment.isMarkedAsDeleted()) {
+            throw new IllegalArgumentException("삭제 처리된 댓글입니다.");
+        }
+
         return comment;
     }
 
