c34df3931a79903e3dd047f09e97a6cad085ee76
imWhS <heextory@icloud.com>
2024-09-26 22:11:03 +0900

    feat: 평가 관련 조회 서비스 계층 메서드 구현

--- src/main/java/imwhs/eatz_server/repository/RecipeRepository.java
+++ src/main/java/imwhs/eatz_server/repository/RecipeRepository.java
@@ -1,9 +1,7 @@
 package imwhs.eatz_server.repository;
 
-import imwhs.eatz_server.domain.EatzUser;
 import imwhs.eatz_server.domain.Recipe;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
