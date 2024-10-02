e2184a1a135d73f77759b17aa4246e3ae791bb04
imWhS <heextory@icloud.com>
2024-10-02 22:40:38 +0900

    feat: 활동 요약 정보를 포함하는 모든 사용자 조회 리포지토리 로직 추가

--- src/main/java/imwhs/eatz_server/repository/eatzuser/query/EatzUserQueryRepository.java
+++ src/main/java/imwhs/eatz_server/repository/eatzuser/query/EatzUserQueryRepository.java
@@ -1,7 +1,9 @@
 package imwhs.eatz_server.repository.eatzuser.query;
 
+import imwhs.eatz_server.dto.eatzuser.EatzUserActivityDto;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +13,16 @@ import java.util.List;
 public class EatzUserQueryRepository {
 
     private final EntityManager em;
+
+    public List<EatzUserActivityDto> findAllWithActivity(Pageable pageable) {
+        return em.createQuery(
+                "select new imwhs.eatz_server.dto.eatzuser.EatzUserActivityDto(" +
+                        "e.id, e.username, count(r)) from EatzUser e " +
+                        "join e.recipes r " +
+                        "group by e.id", EatzUserActivityDto.class)
+                .setFirstResult((int) pageable.getOffset())
+                .setMaxResults(pageable.getPageSize())
+                .getResultList();
+    }
 
 }
