768dbe702b9df02de0e8e94506297c4677772da9
imWhS <heextory@icloud.com>
2024-10-06 17:35:54 +0900

    feat: 활동 요약 정보를 포함하는 모든 사용자 조회 로직 수정 및 테스트 추가

--- src/main/java/imwhs/eatz_server/repository/eatzuser/query/EatzUserQueryRepository.java
+++ src/main/java/imwhs/eatz_server/repository/eatzuser/query/EatzUserQueryRepository.java
@@ -1,9 +1,9 @@
 package imwhs.eatz_server.repository.eatzuser.query;
 
-import imwhs.eatz_server.dto.eatzuser.EatzUserActivityDto;
+import imwhs.eatz_server.dto.Paged;
+import imwhs.eatz_server.dto.eatzuser.EatzUserActivityResponseDto;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,15 +14,23 @@ public class EatzUserQueryRepository {
 
     private final EntityManager em;
 
-    public List<EatzUserActivityDto> findAllWithActivity(Pageable pageable) {
-        return em.createQuery(
-                "select new imwhs.eatz_server.dto.eatzuser.EatzUserActivityDto(" +
-                        "e.id, e.username, count(r)) from EatzUser e " +
-                        "join e.recipes r " +
-                        "group by e.id", EatzUserActivityDto.class)
-                .setFirstResult((int) pageable.getOffset())
-                .setMaxResults(pageable.getPageSize())
+    public Paged<EatzUserActivityResponseDto> findAllWithActivity(int page, int size) {
+        List<EatzUserActivityResponseDto> items = em.createQuery(
+                        "select new imwhs.eatz_server.dto.eatzuser.EatzUserActivityResponseDto(" +
+                                "e.id, e.username, count(r)) from EatzUser e " +
+                                "left join e.recipes r " +
+                                "group by e.id", EatzUserActivityResponseDto.class)
+                .setFirstResult(page * size)
+                .setMaxResults(size)
                 .getResultList();
+
+        Long totalItems = em.createQuery(
+                "select count(e.id) from EatzUser e", Long.class
+        ).getSingleResult();
+
+        int totalPages = (int) Math.ceil((double) totalItems / size);
+
+        return Paged.of(items, totalItems, totalPages, page, size);
     }
 
 }
