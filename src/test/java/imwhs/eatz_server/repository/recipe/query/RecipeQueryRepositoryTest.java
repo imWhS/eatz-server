aa09c958bde2c53f61adf418355557ee7017e883
imWhS <heextory@icloud.com>
2024-10-14 23:54:53 +0900

    feat: 레시피 상세 조회 기능 개발

--- src/test/java/imwhs/eatz_server/repository/recipe/query/RecipeQueryRepositoryTest.java
+++ src/test/java/imwhs/eatz_server/repository/recipe/query/RecipeQueryRepositoryTest.java
@@ -38,43 +38,50 @@ class RecipeQueryRepositoryTest {
     @Transactional
     void findRecipeDetailByIdTest() {
         // given
-        EatzUser user = EatzUser.create("heextory1", "heextory1@icloud.com", "1q2w3e4r!", Role.MEMBER);
-        userRepository.save(user);
+        EatzUser userA = EatzUser.create("heextory1", "heextory1@icloud.com", "1q2w3e4r!", Role.MEMBER);
+        userRepository.save(userA);
 
-        Recipe recipe1 = Recipe.create(user, "Kimchi pasta", "https://www.naver.com", "https://imgcdn.naver.com", "맛있는 김치 파스타를 즐겨보세요!");
+        Recipe recipe1 = Recipe.create(userA, "Kimchi pasta", "https://www.naver.com", "https://imgcdn.naver.com", "맛있는 김치 파스타를 즐겨보세요!");
         recipeRepository.save(recipe1);
         Long recipeId = recipe1.getId();
-
-        Recipe recipe2 = Recipe.create(user, "22Kimchi pasta", "https://www.naver.com", "https://imgcdn.naver.com", "맛있는 김치 파스타를 즐겨보세요!");
-        recipeRepository.save(recipe2);
-
-        Recipe recipe3 = Recipe.create(user, "333Kimchi pasta", "https://www.naver.com", "https://imgcdn.naver.com", "맛있는 김치 파스타를 즐겨보세요!");
-        recipeRepository.save(recipe3);
 
         EatzUser userB = EatzUser.create("heextory2", "heextory2@icloud.com", "1q2w3e4r!", Role.MEMBER);
         userRepository.save(userB);
 
+        // 사용자 B가 댓글 및 평가를 남김
         Comment comment1 = new Comment(userB, recipe1, "내 맘 속에 저장~");
         commentRepository.save(comment1);
 
         Comment comment2 = new Comment(userB, recipe1, "내 맘 속에 두 번째로 저장~");
         commentRepository.save(comment2);
 
-        Comment comment3 = new Comment(userB, recipe1, "내 맘 속에 마지막으로 저장~");
-        commentRepository.save(comment3);
+        Rating rating1 = new Rating(userB, recipe1, 5, "최근 먹은 것 중 최고의 한 끼!");
+        ratingRepository.save(rating1);
 
-        Rating rating = new Rating(userB, recipe1, 5, null);
-        ratingRepository.save(rating);
+        // 사용자 C가 recipe1에 추가 평가를 남김
+        EatzUser userC = EatzUser.create("heextory3", "heextory3@icloud.com", "1q2w3e4r!", Role.MEMBER);
+        userRepository.save(userC);
+
+        Rating rating2 = new Rating(userC, recipe1, 4, "맛있지만 조금 짰어요.");
+        ratingRepository.save(rating2);
+
+        // 사용자 D가 recipe1에 추가 평가를 남김
+        EatzUser userD = EatzUser.create("heextory4", "heextory4@icloud.com", "1q2w3e4r!", Role.MEMBER);
+        userRepository.save(userD);
+
+        Rating rating3 = new Rating(userD, recipe1, 3, "쏘 쏘 ㅋ");
+        ratingRepository.save(rating3);
 
         // when
         RecipeDetailResponseDto recipeDetail = recipeQueryRepository.findRecipeDetailById(recipeId);
 
         // then
-        Assertions.assertNotNull(recipeDetail);
-        Assertions.assertEquals(recipeId, recipeDetail.getId());
-        Assertions.assertEquals(user.getUsername(), recipeDetail.getUser().getUsername());
-        Assertions.assertEquals(3, recipeDetail.getUser().getRecipeCount());
-
+        assertNotNull(recipeDetail);
+        assertEquals(recipeId, recipeDetail.getId());
+        assertEquals("Kimchi pasta", recipeDetail.getTitle());
+        assertEquals(2, recipeDetail.getCommentCount());
+        assertEquals(3, recipeDetail.getRatingSummary().getRatingCount());
+        assertEquals(4, recipeDetail.getRatingSummary().getAverageRatingScore(), 0.01);
     }
 
 }
