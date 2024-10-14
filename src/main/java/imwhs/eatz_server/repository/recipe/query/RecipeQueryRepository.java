29665a9bee7ed1c9410661291b7e2062e575960a
imWhS <heextory@icloud.com>
2024-10-14 23:54:53 +0900

    feat: 레시피 상세 조회 기능 개발

--- src/main/java/imwhs/eatz_server/repository/recipe/query/RecipeQueryRepository.java
+++ src/main/java/imwhs/eatz_server/repository/recipe/query/RecipeQueryRepository.java
@@ -1,12 +1,10 @@
 package imwhs.eatz_server.repository.recipe.query;
 
+import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import imwhs.eatz_server.domain.QComment;
-import imwhs.eatz_server.domain.QEatzUser;
-import imwhs.eatz_server.domain.QRating;
-import imwhs.eatz_server.domain.QRecipe;
+import imwhs.eatz_server.domain.*;
 import imwhs.eatz_server.dto.eatzuser.EatzUserSummaryDto;
 import imwhs.eatz_server.dto.rating.RatingSummaryDto;
 import imwhs.eatz_server.dto.recipe.RecipeDetailResponseDto;
@@ -18,54 +16,9 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class RecipeQueryRepository {
 
-    private final EntityManager em;
-
     private final JPAQueryFactory queryFactory;
 
     public RecipeDetailResponseDto findRecipeDetailById(Long id) {
-        /**
-         * id에 대한 Recipe를 먼저 조회하되, Recipe와 연관 관계 설정된 EatzUser, Comment, Rating을
-         * LEFT JOIN해서 한 번에 가져온다.
-         */
-
-        QRecipe recipe = QRecipe.recipe;
-        QRecipe subRecipe = QRecipe.recipe;
-        QEatzUser user = QEatzUser.eatzUser;
-        QComment comment = QComment.comment;
-        QRating rating = QRating.rating;
-
-        return queryFactory
-                .select(
-                        Projections.constructor(RecipeDetailResponseDto.class, // RecipeDetailResponseDto로 프로젝션합니다.
-                                recipe.id,
-                                Projections.constructor(EatzUserSummaryDto.class, // EatzUserSummaryDto로 프로젝션합니다.
-                                        user.id,
-                                        user.username,
-                                        JPAExpressions // 해당 레시피를 등록한 사용자가 레시피 앱에 등록한 모든 레시피 개수를 가져오는 서브 쿼리
-                                                .select(subRecipe.count())
-                                                .from(subRecipe) // user에 해당하는 모든 recipe를 조회합니다.
-                                                .join(subRecipe.user, user)
-                                                .where(subRecipe.user.eq(user))),
-                                recipe.title,
-                                recipe.url,
-                                recipe.imageUrl,
-                                recipe.description,
-                                comment.count().intValue(),
-                                Projections.constructor(RatingSummaryDto.class, // RatingSummaryDto로 프로젝션합니다.
-                                        rating.count().intValue(),
-                                        JPAExpressions
-                                                .select(rating.score.avg())
-                                                .from(rating)
-                                                .where(rating.recipe.eq(recipe)))))
-                .from(recipe) // id에 해당하는 recipe를 조회합니다.
-                .leftJoin(recipe.user, user) // 해당 recipe와 연관 관계에 있는 user, comment, rating을 join해 함께 가져옵니다.
-                .leftJoin(recipe.comments, comment)
-                .leftJoin(recipe.ratings, rating)
-                .where(recipe.id.eq(id))
-                .fetchOne();
-    }
-
-    public RecipeDetailResponseDto findRecipeDetailById2(Long id) {
         QRecipe recipe = QRecipe.recipe;
         QEatzUser user = QEatzUser.eatzUser;
         QComment comment = QComment.comment;
@@ -81,32 +34,25 @@ public class RecipeQueryRepository {
                                         JPAExpressions
                                                 .select(recipe.count())
                                                 .from(recipe)
-                                                .where(recipe.user.eq(user))
-                                        ),
+                                                .where(recipe.user.id.eq(user.id))),
                                 recipe.title,
                                 recipe.url,
                                 recipe.imageUrl,
                                 recipe.description,
-                                JPAExpressions
-                                        .select(comment.count())
-                                        .from(comment)
-                                        .where(comment.recipe.eq(recipe)),
+                                comment.id.countDistinct().intValue(),
                                 Projections.constructor(RatingSummaryDto.class,
-                                        JPAExpressions
-                                                .select(rating.count())
-                                                .from(rating)
-                                                .where(rating.recipe.eq(recipe)),
-                                        JPAExpressions
-                                                .select(rating.score.avg())
-                                                .from(rating)
-                                                .where(rating.recipe.eq(recipe))
-                                        )
-                                )
+                                        // 사용자는 레시피에 하나의 평가만 남길 수 있기 때문에, 평가 식별자 값 기준으로 distinct를 적용합니다.
+                                        rating.countDistinct().intValue(),
+                                        rating.score.avg())
+                        )
                 )
                 .from(recipe)
-                .leftJoin(recipe.user, user) // Recipe와 EatzUser를 LEFT JOIN 합니다.
+                .leftJoin(recipe.user, user)
+                .leftJoin(recipe.comments, comment)
+                .leftJoin(recipe.ratings, rating)
                 .where(recipe.id.eq(id))
-                .fetchFirst();
+                .groupBy(recipe.id)
+                .fetchOne();
     }
 
 }
