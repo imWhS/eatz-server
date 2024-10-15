a16c1b1fb5607040259944a428df28b3a3b772de
imWhS <heextory@icloud.com>
2024-10-15 17:10:42 +0900

    test: 레시피 상세 정보 조회 테스트 코드 변경

--- src/test/java/imwhs/eatz_server/repository/recipe/query/RecipeQueryRepositoryTest.java
+++ src/test/java/imwhs/eatz_server/repository/recipe/query/RecipeQueryRepositoryTest.java
@@ -36,41 +36,38 @@ class RecipeQueryRepositoryTest {
 
     @Test
     @Transactional
-    void findRecipeDetailByIdTest() {
-        // given
+    void findRecipeDetailById_MultipleUsersCommentsAndRatings() {
+        // given: 사용자 A가 레시피를 등록합니다.
         EatzUser userA = EatzUser.create("heextory1", "heextory1@icloud.com", "1q2w3e4r!", Role.MEMBER);
         userRepository.save(userA);
 
-        Recipe recipe1 = Recipe.create(userA, "Kimchi pasta", "https://www.naver.com", "https://imgcdn.naver.com", "맛있는 김치 파스타를 즐겨보세요!");
+        Recipe recipe1 = Recipe.create(userA, "Pasta", "https://www.naver.com", "https://imgcdn.naver.com", "맛있는 파스타를 즐겨보세요!");
         recipeRepository.save(recipe1);
         Long recipeId = recipe1.getId();
 
+        // given: 사용자 B, C, D가 여러 댓글과 평가를 남깁니다.
         EatzUser userB = EatzUser.create("heextory2", "heextory2@icloud.com", "1q2w3e4r!", Role.MEMBER);
         userRepository.save(userB);
-
-        // 사용자 B가 댓글 및 평가를 남김
-        Comment comment1 = new Comment(userB, recipe1, "내 맘 속에 저장~");
-        commentRepository.save(comment1);
-
-        Comment comment2 = new Comment(userB, recipe1, "내 맘 속에 두 번째로 저장~");
-        commentRepository.save(comment2);
-
-        Rating rating1 = new Rating(userB, recipe1, 5, "최근 먹은 것 중 최고의 한 끼!");
-        ratingRepository.save(rating1);
-
-        // 사용자 C가 recipe1에 추가 평가를 남김
         EatzUser userC = EatzUser.create("heextory3", "heextory3@icloud.com", "1q2w3e4r!", Role.MEMBER);
         userRepository.save(userC);
-
-        Rating rating2 = new Rating(userC, recipe1, 4, "맛있지만 조금 짰어요.");
-        ratingRepository.save(rating2);
-
-        // 사용자 D가 recipe1에 추가 평가를 남김
         EatzUser userD = EatzUser.create("heextory4", "heextory4@icloud.com", "1q2w3e4r!", Role.MEMBER);
         userRepository.save(userD);
 
-        Rating rating3 = new Rating(userD, recipe1, 3, "쏘 쏘 ㅋ");
-        ratingRepository.save(rating3);
+        // given: 사용자 B가 3개의 댓글과 2개의 평점을 남깁니다.
+        commentRepository.save(new Comment(userB, recipe1, "정말 맛있어요!"));
+        commentRepository.save(new Comment(userB, recipe1, "다시 먹고 싶어요."));
+        commentRepository.save(new Comment(userB, recipe1, "추천합니다."));
+        ratingRepository.save(new Rating(userB, recipe1, 5, "최고의 맛!"));
+        ratingRepository.save(new Rating(userB, recipe1, 4, "조금 짰어요."));
+
+        // given: 사용자 C가 2개의 댓글과 1개의 평점을 남깁니다.
+        commentRepository.save(new Comment(userC, recipe1, "이 레시피는 진짜 최고입니다."));
+        commentRepository.save(new Comment(userC, recipe1, "한 번 더 만들어볼게요."));
+        ratingRepository.save(new Rating(userC, recipe1, 5, "맛있고 간단해요!"));
+
+        // given: 사용자 D가 1개의 댓글과 1개의 평점을 남깁니다.
+        commentRepository.save(new Comment(userD, recipe1, "아주 맛있습니다!"));
+        ratingRepository.save(new Rating(userD, recipe1, 3, "무난한 맛이네요."));
 
         // when
         RecipeDetailResponseDto recipeDetail = recipeQueryRepository.findRecipeDetailById(recipeId);
@@ -78,10 +75,10 @@ class RecipeQueryRepositoryTest {
         // then
         assertNotNull(recipeDetail);
         assertEquals(recipeId, recipeDetail.getId());
-        assertEquals("Kimchi pasta", recipeDetail.getTitle());
-        assertEquals(2, recipeDetail.getCommentCount());
-        assertEquals(3, recipeDetail.getRatingSummary().getRatingCount());
-        assertEquals(4, recipeDetail.getRatingSummary().getAverageRatingScore(), 0.01);
+        assertEquals("Pasta", recipeDetail.getTitle());
+        assertEquals(6, recipeDetail.getCommentCount());
+        assertEquals(4, recipeDetail.getRatingSummary().getRatingCount());
+        assertEquals(4.25, recipeDetail.getRatingSummary().getAverageRatingScore(), 0.01);
     }
 
 }
