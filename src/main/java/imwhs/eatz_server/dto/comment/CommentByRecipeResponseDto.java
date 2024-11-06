2e3fdd10553c6bb67ec902bb3fb0a54fb56db721
imWhS <heextory@icloud.com>
2024-11-06 18:37:23 +0900

    feat: 사용자가 등록한 모든 댓글 조회

--- src/main/java/imwhs/eatz_server/dto/comment/CommentByRecipeResponseDto.java
+++ src/main/java/imwhs/eatz_server/dto/comment/CommentByRecipeResponseDto.java
@@ -2,20 +2,23 @@ package imwhs.eatz_server.dto.comment;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import imwhs.eatz_server.domain.Comment;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 /**
- * CommentResponseDto 클래스입니다.
+ * CommentByRecipeResponseDto 클래스입니다.
  * <ul>
  *     <li>댓글의 기본 정보와 댓글을 작성한 사용자의 기본(최소) 정보를 포함하는 DTO입니다.</li>
- *     <li>댓글 목록과 같이 댓글이 컬렉션에 포함되어졌을 때, 컬렉션 내 모든 댓글을 보다 효율적으로 조회해야 하는 경우에 주로 사용합니다.</li>
+ *     <li>레시피 별 댓글 목록을 조회하는 상황과 같이, 댓글 목록과 같이 댓글이 컬렉션에 포함되어졌을 때<br/>
+ *     컬렉션 내 모든 댓글을 보다 효율적으로 조회해야 하는 경우에 주로 사용합니다.</li>
  * </ul>
  */
 // TODO: DTO 클래스 공통 필드 상속
 @Data
-public class CommentResponseDto {
+@AllArgsConstructor
+public class CommentByRecipeResponseDto {
 
     private Long id;
 
@@ -35,7 +38,7 @@ public class CommentResponseDto {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime deletedAt;
 
-    public CommentResponseDto(
+    public CommentByRecipeResponseDto(
             Long id,
             CommentUserDto user,
             String content,
@@ -46,7 +49,7 @@ public class CommentResponseDto {
         this.isHidden = isHidden;
     }
 
-    public CommentResponseDto(Comment comment) {
+    public CommentByRecipeResponseDto(Comment comment) {
         this.id = comment.getId();
         this.user = new CommentUserDto(comment.getUser());
         this.content = comment.getContent();
---
+++ src/main/java/imwhs/eatz_server/dto/comment/CommentByUserResponseDto.java
@@ -0,0 +1,62 @@
+package imwhs.eatz_server.dto.comment;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import imwhs.eatz_server.domain.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * CommentByUserResponseDto 클래스입니다.
+ * <ul>
+ *     <li>댓글의 기본 정보와 댓글이 달린 레시피의 기본(최소) 정보를 포함하는 DTO입니다.</li>
+ *     <li>사용자가 등록한 댓글 목록을 조회하는 상황과 같이, 댓글 목록과 같이 댓글이 컬렉션에 포함되어졌을 때<br/>
+ *     컬렉션 내 모든 댓글을 보다 효율적으로 조회해야 하는 경우에 주로 사용합니다.</li>
+ * </ul>
+ */
+// TODO: DTO 클래스 공통 필드 상속
+@Data
+@AllArgsConstructor
+public class CommentByUserResponseDto {
+
+    private Long id;
+
+    /** 댓글이 달린 레시피의 기본 정보 */
+    private CommentRecipeDto recipe;
+
+    private String content;
+
+    private boolean isHidden;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updatedAt;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime deletedAt;
+
+    public CommentByUserResponseDto(
+            Long id,
+            CommentRecipeDto recipe,
+            String content,
+            boolean isHidden) {
+        this.id = id;
+        this.recipe = this.recipe;
+        this.content = content;
+        this.isHidden = isHidden;
+    }
+
+    public CommentByUserResponseDto(Comment comment) {
+        this.id = comment.getId();
+        this.recipe = new CommentRecipeDto(comment.getRecipe());
+        this.content = comment.getContent();
+        this.isHidden = comment.isHidden();
+        this.createdAt = comment.getCreatedAt();
+        this.updatedAt = comment.getUpdatedAt();
+        this.deletedAt = comment.getDeletedAt();
+    }
+
+}
