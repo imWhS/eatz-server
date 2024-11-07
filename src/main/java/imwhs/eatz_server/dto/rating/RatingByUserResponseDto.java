01a395d0f846fa9d5c293d33551bbaa900e80922
imWhS <heextory@icloud.com>
2024-11-07 23:58:54 +0900

    feat: Rating 조회 메서드

---
+++ src/main/java/imwhs/eatz_server/dto/rating/RatingByUserResponseDto.java
@@ -0,0 +1,69 @@
+package imwhs.eatz_server.dto.rating;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import imwhs.eatz_server.domain.Comment;
+import imwhs.eatz_server.domain.Rating;
+import imwhs.eatz_server.dto.comment.CommentRecipeDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * RatingByUserResponseDto 클래스입니다.
+ * <ul>
+ *     <li>평가의 기본 정보와 평가가 달린 레시피의 기본(최소) 정보를 포함하는 DTO입니다.</li>
+ *     <li>사용자가 등록한 평가 목록을 조회하는 상황에서, 평가 목록과 같이 평가가 컬렉션에 포함되어졌을 때<br/>
+ *     컬렉션 내 모든 평가를 보다 효율적으로 조회해야 하는 경우에 주로 사용합니다.</li>
+ * </ul>
+ */
+// TODO: DTO 클래스 공통 필드 상속
+@Data
+@AllArgsConstructor
+public class RatingByUserResponseDto {
+
+    private Long id;
+
+    /** 평가가 달린 레시피의 기본 정보 */
+    private RatingRecipeDto recipe;
+
+    private Integer score;
+
+    private String content;
+
+    private boolean isHidden;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updatedAt;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime deletedAt;
+
+    public RatingByUserResponseDto(
+            Long id,
+            RatingRecipeDto recipe,
+            Integer score,
+            String content,
+            boolean isHidden) {
+        this.id = id;
+        this.recipe = recipe;
+        this.score = score;
+        this.content = content;
+        this.isHidden = isHidden;
+    }
+
+    public RatingByUserResponseDto(Rating rating) {
+        this.id = rating.getId();
+        this.recipe = new RatingRecipeDto(rating.getRecipe());
+        this.score = rating.getScore();
+        this.content = rating.getContent();
+        this.isHidden = rating.isHidden();
+        this.createdAt = rating.getCreatedAt();
+        this.updatedAt = rating.getUpdatedAt();
+        this.deletedAt = rating.getDeletedAt();
+    }
+
+}
---
+++ src/main/java/imwhs/eatz_server/repository/rating/RatingQueryRepository.java
@@ -0,0 +1,179 @@
+package imwhs.eatz_server.repository.rating;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import imwhs.eatz_server.domain.QEatzUser;
+import imwhs.eatz_server.domain.QRating;
+import imwhs.eatz_server.domain.QRecipe;
+import imwhs.eatz_server.dto.eatzuser.EatzUserSummaryDto;
+import imwhs.eatz_server.dto.rating.*;
+import imwhs.eatz_server.dto.recipe.RecipeSummaryDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class RatingQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    /**
+     * 1. 평가 상세 조회
+     * 2. 레시피에 달린 모든 평가 조회
+     * 3. 레시피에 달린 총 평가 수 조회
+     * 4. 특정 사용자가 등록한 모든 평가 조회
+     * 5. 특정 사용자가 등록한 모든 평가 수 조회
+     */
+
+    /**
+     * 식별자에 해당하는 평가의 기본 정보 및 평가를 등록한 사용자와 레시피 부가 정보를 함께 조회합니다.<br/>
+     * 삭제 처리된 평가는 조회 대상에서 제외됩니다.
+     * @param id 평가 식별자
+     * @return Optional로 wrapping된 RatingDetailResponseDto. 평가의 상세 정보를 담은 DTO입니다.
+     */
+    public Optional<RatingDetailResponseDto> findRatingDetailById(Long id) {
+        QRating rating = QRating.rating;
+        QEatzUser user = QEatzUser.eatzUser;
+        QRecipe recipe = QRecipe.recipe;
+
+        return Optional.ofNullable(queryFactory
+                .select(
+                        Projections.constructor(RatingDetailResponseDto.class,
+                                rating.id,
+                                Projections.constructor(EatzUserSummaryDto.class,
+                                        user.id,
+                                        user.username,
+                                        JPAExpressions
+                                                .select(recipe.count().intValue())
+                                                .from(recipe)
+                                                .where(recipe.user.eq(user))
+                                ),
+                                Projections.constructor(RecipeSummaryDto.class,
+                                        recipe.id,
+                                        recipe.title,
+                                        recipe.imageUrl
+                                ),
+                                rating.score,
+                                rating.content
+                        )
+                )
+                .from(rating)
+                .leftJoin(rating.user, user)
+                .leftJoin(rating.recipe, recipe)
+                .where(rating.id.eq(id)
+                        .and(rating.deletedAt.isNull()))
+                .fetchOne()
+        );
+    }
+
+    /**
+     * 특정 레시피에 달린 모든 평가를 조회합니다.<br/>
+     * 평가 별 기본 정보 뿐 아니라 해당 평가를 등록한 사용자의 부가 정보를 함께 조회합니다.<br/>
+     * 삭제 처리된 평가는 조회 대상에서 제외됩니다.
+     * @param id 레시피 식별자.
+     * @param page 페이징 처리 시, 조회할 페이지 인덱스. 0부터 시작하며 선택 사항입니다.
+     * @param size 페이징 처리 시, 하나의 페이지에 포함할 레시피 수. 선택 사항입니다.
+     */
+    public List<RatingByRecipeResponseDto> findRatingsByRecipe(Long id, int page, int size) {
+        QRating rating = QRating.rating;
+        QEatzUser user = QEatzUser.eatzUser;
+
+        return queryFactory.select(
+                Projections.constructor(RatingByRecipeResponseDto.class,
+                        rating.id,
+                        Projections.constructor(RatingUserDto.class,
+                                user.id,
+                                user.username
+                        ),
+                        rating.score,
+                        rating.content,
+                        rating.isHidden,
+                        rating.createdAt,
+                        rating.updatedAt,
+                        rating.deletedAt)
+                )
+                .from(rating)
+                .leftJoin(rating.user, user)
+                .where(rating.user.eq(user)
+                        .and(rating.deletedAt.isNull())
+                )
+                .offset((long) page * size)
+                .limit(size)
+                .fetch();
+    }
+
+    /**
+     * 특정 레시피에 달린 총 평가 수를 조회합니다.<br/>
+     * 삭제 처리된 평가는 조회 대상에서 제외합니다.
+     * @param id 레시피 식별자
+     * @return 총 평가 수
+     */
+    public Long countRatingsByRecipe(Long id) {
+        QRating rating = QRating.rating;
+
+        return queryFactory.select(rating.count())
+                .from(rating)
+                .where(rating.recipe.id.eq(id)
+                        .and(rating.deletedAt.isNull()))
+                .fetchOne();
+
+    }
+
+    /**
+     * 특정 사용자가 등록한 모든 평가를 조회합니다.<br/>
+     * 평가 별 기본 정보 뿐 아니라 해당 평가를 등록한 사용자의 부가 정보를 함께 조회합니다.<br/>
+     * 삭제 처리된 평가는 조회 대상에서 제외됩니다.
+     * @param id 사용자 식별자
+     * @param page 페이징 처리 시, 조회할 페이지 인덱스. 0부터 시작하며 선택 사항입니다.
+     * @param size 페이징 처리 시, 하나의 페이지에 포함할 레시피 수. 선택 사항입니다.
+     */
+    public List<RatingByUserResponseDto> findRatingsByUser(Long id, int page, int size) {
+        QRating rating = QRating.rating;
+        QRecipe recipe = QRecipe.recipe;
+
+        return queryFactory.select(
+                Projections.constructor(RatingByUserResponseDto.class,
+                        rating.id,
+                        Projections.constructor(RatingRecipeDto.class,
+                                recipe.id,
+                                recipe.title,
+                                recipe.description
+                        ),
+                        rating.score,
+                        rating.content,
+                        rating.isHidden,
+                        rating.createdAt,
+                        rating.updatedAt,
+                        rating.deletedAt)
+                )
+                .from(rating)
+                .leftJoin(rating.recipe, recipe)
+                .where(rating.user.id.eq(id))
+                .offset((long) page * size)
+                .limit(size)
+                .fetch();
+    }
+
+    /**
+     * 특정 사용자가 등록한 총 평가 수를 조회합니다.<br/>
+     * 삭제 처리된 평가는 조회 대상에서 제외합니다.
+     * @param id 사용자 식별자
+     * @return 총 평가 수
+     */
+    public Long countRatingsByUser(Long id) {
+        QRating rating = QRating.rating;
+
+        return queryFactory.select(rating.count())
+                .from(rating)
+                .where(rating.user.id.eq(id)
+                        .and(rating.deletedAt.isNull()))
+                .fetchOne();
+    }
+
+
+
+}
