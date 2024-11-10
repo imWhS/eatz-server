9729a487588a9a7be4c6fe544c2983bf6009304a
imWhS <heextory@icloud.com>
2024-11-10 18:19:16 +0900

    feat: 서비스 계층 내 조회 메서드 및 관련 DTO 클래스 구현

---
+++ src/main/java/imwhs/eatz_server/dto/ingredient/IngredientCategoryResponseDto.java
@@ -0,0 +1,12 @@
+package imwhs.eatz_server.dto.ingredient;
+
+import lombok.Data;
+
+@Data
+public class IngredientCategoryResponseDto {
+
+    private Long categoryId;
+
+    private String categoryName;
+
+}
---
+++ src/main/java/imwhs/eatz_server/dto/ingredient/IngredientChildResponseDto.java
@@ -0,0 +1,12 @@
+package imwhs.eatz_server.dto.ingredient;
+
+import lombok.Data;
+
+@Data
+public class IngredientChildResponseDto {
+
+    private Long id;
+
+    private String name;
+
+}
