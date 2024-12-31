ded1a6a250fecb50b17499d1d46faa7c7139059c
imWhS <heextory@icloud.com>
2024-12-31 18:52:41 +0900

    feat: 유효하지 않은 토큰인 경우 401 코드 응답 처리

--- src/main/java/imwhs/eatz_server/auth/TokenManager.java
+++ src/main/java/imwhs/eatz_server/auth/TokenManager.java
@@ -15,7 +15,7 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * JWT 형식의 토큰 생성과 토큰 내용 파싱 등과 관련한 작업을 담당하는 클래스입니다.
+ * JWT 형식의 토큰 발급, 토큰 파싱 등과 관련한 작업을 담당하는 클래스입니다.
  */
 @RequiredArgsConstructor
 @Component
@@ -28,7 +28,7 @@ public class TokenManager {
     }
 
     /**
-     * 사용자 인증 및 인가 정보를 담은 토큰을 생성합니다.
+     * 사용자 인증 및 인가 정보를 담은 토큰을 발급합니다.
      * @param email 사용자 이메일
      * @param role 사용자 권한
      * @return 토큰
@@ -51,13 +51,15 @@ public class TokenManager {
     }
 
     /**
-     * 토큰의 payload 정보를 Claims 객체로 반환합니다.
+     * 토큰의 유효성을 검증하고, 토큰의 payload 정보를 Claims 객체로 반환합니다.
      * @param token 토큰.
      * @return Claims 객체.
      */
     public Claims parseClaims(String token) {
+        // 비밀 키로 토큰의 유효성을 검증합니다.
         SecretKey key = Keys.hmacShaKeyFor(jwtProperties.getSecretKey().getBytes());
 
+        // 토큰을 파싱합니다.
         return Jwts.parserBuilder()
                 .setSigningKey(key)
                 .build()
@@ -66,12 +68,15 @@ public class TokenManager {
     }
 
     /**
-     * 토큰으로 인증을 위한 객체 Authentication을 생성합니다.
+     * 토큰으로 Authentication을 생성합니다.
      * @param token 토큰.
      * @return Authentication 객체.
      */
     public Authentication createAuthentication(String token) {
+        // 토큰을 검증한 후 파싱합니다.
         Claims claims = parseClaims(token);
+
+        // 토큰에서 이메일 주소와 역할 정보를 가져옵니다.
         String email = claims.getSubject();
         String role = claims.get("role", String.class);
 
