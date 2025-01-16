feadcfeccbad1b3dbcb7d77817d746fe9e6aea3d
imWhS <heextory@icloud.com>
2025-01-16 23:59:02 +0900

    feat: 이미지 업로드 컨트롤러 기초 구현

--- src/main/java/imwhs/eatz_server/config/WebMvcConfig.java
+++ src/main/java/imwhs/eatz_server/config/WebMvcConfig.java
@@ -1,11 +1,14 @@
 package imwhs.eatz_server.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+@Slf4j
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
@@ -14,4 +17,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
         resolvers.add(new PageableValidationHandlerMethodArgumentResolver());
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("addResourceHandlers가 호출됐어요!");
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:/Users/son-wonhui/Library/Mobile Documents/com~apple~CloudDocs/dev_whs/Eatz/uploads/");
+    }
 }
---
+++ src/main/java/imwhs/eatz_server/config/properties/EatzServerProperties.java
@@ -0,0 +1,16 @@
+package imwhs.eatz_server.config.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "eatz.server")
+public class EatzServerProperties {
+
+    private String baseUrl;
+
+}
