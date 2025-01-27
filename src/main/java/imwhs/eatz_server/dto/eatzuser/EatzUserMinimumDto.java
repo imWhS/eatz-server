75f5c141d3f72bc010ecf40ae5c9bade1cd412be
imWhS <heextory@icloud.com>
2025-01-28 01:35:13 +0900

    feat: 레시피 조회 API 별 좋아요 수, 사용자 정보 추가 및 특정 사용자의 모든 레시피 조회 API 추가

--- src/main/java/imwhs/eatz_server/dto/eatzuser/EatzUserMinimumDto.java
+++ src/main/java/imwhs/eatz_server/dto/eatzuser/EatzUserMinimumDto.java
@@ -1,18 +1,14 @@
-package imwhs.eatz_server.dto;
+package imwhs.eatz_server.dto.eatzuser;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import imwhs.eatz_server.domain.EatzUser;
-import imwhs.eatz_server.domain.Role;
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 /**
- * LikedUserDto 클래스입니다.<br/>
- * 좋아요한 사용자 정보를 전달하기 위해 사용합니다.
+ * EatzUserMinimumDto 클래스입니다.<br/>
+ * 사용자에 대한 간략한 정보를 전달하기 위해 사용합니다.
  */
 @Data
-public class LikedUserDto {
+public class EatzUserMinimumDto {
 
     private Long id;
 
@@ -22,7 +18,7 @@ public class LikedUserDto {
 
     private String imageUrl;
 
-    public LikedUserDto(EatzUser user) {
+    public EatzUserMinimumDto(EatzUser user) {
         this.id = user.getId();
         this.username = user.getUsername();
         this.email = user.getEmail();
