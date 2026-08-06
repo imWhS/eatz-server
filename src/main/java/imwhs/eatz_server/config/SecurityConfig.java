7250f4d68740e9d087ae6a6065a4dc23e5ba9a8c
imWhS <heextory@icloud.com>
2026-08-06 10:47:46 +0900

    fix: /system을 public urls에 추가

--- src/main/java/imwhs/eatz_server/config/SecurityConfig.java
+++ src/main/java/imwhs/eatz_server/config/SecurityConfig.java
@@ -56,6 +56,7 @@ public class SecurityConfig {
 
     private static final String[] PUBLIC_URLS = {
             "/",
+            "/system/**",
             "/error",
             "/api/v0/auth/**",
             "/login",
