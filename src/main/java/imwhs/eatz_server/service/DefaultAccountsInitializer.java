a71d310a11ee499b5df27bf404f98f4ad1761c96
imWhS <heextory@icloud.com>
2026-08-07 17:45:00 +0900

    fix: DataInitRunner의 일부 데이터 수정 및 ApplicationRunner 컴포넌트 실행 순서 지정

--- src/main/java/imwhs/eatz_server/service/DefaultAccountsInitializer.java
+++ src/main/java/imwhs/eatz_server/service/DefaultAccountsInitializer.java
@@ -7,9 +7,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+@Order(1)
 @Slf4j
 @RequiredArgsConstructor
 @Component
