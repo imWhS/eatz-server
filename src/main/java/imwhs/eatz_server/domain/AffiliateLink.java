bda10ca45ac4cfd78f2215ad4f8173fcac1be524
imWhS <heextory@icloud.com>
2026-09-22 19:37:06 +0900

    feat: AffiliateLink 엔티티 클래스에 updateUrl() 추가

--- src/main/java/imwhs/eatz_server/domain/AffiliateLink.java
+++ src/main/java/imwhs/eatz_server/domain/AffiliateLink.java
@@ -1,14 +1,17 @@
 package imwhs.eatz_server.domain;
 
+import imwhs.eatz_server.exception.EatzInvalidRequestArgumentException;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Table(name = "affiliate_link", uniqueConstraints = {
         @UniqueConstraint(name = "uk_requirement_type_requirement_id", columnNames = {"requirement_type", "requirement_id"})
 })
@@ -36,6 +39,14 @@ public class AffiliateLink {
         link.requirementId = requirementId;
         link.url = url;
         return link;
+    }
+
+    public void updateUrl(String url) {
+        if (Objects.isNull(url) || url.isBlank()) {
+            throw new EatzInvalidRequestArgumentException("URL이 비어 있거나, 올바르지 않아요.");
+        }
+
+        this.url = url;
     }
 
 }
